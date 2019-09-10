1cb84bfc6d02fb628377211b14042eab80abeb09
Edson Andrez <edson.andrez@outlook.es>
2019-09-10 00:59:41 -0500

    search component.

---
+++ resources/views/components/search.blade.php
@@ -0,0 +1,27 @@
+<div class="row">
+    <div class="ui grid">
+        <div class="eight wide column">
+            <div class="ui category search">
+                <div class="ui action input">
+                    <input
+                        class=""
+                        onkeydown="if(event.keyCode == 13)
+                            ajaxLoad('{{Request::url()}}?search='+this.value+'&sort={{$sort}}&field={{$sortField}} ')"
+                        type="text"
+                        placeholder="Buscar...">
+
+                    <button class="ui icon button"
+                        onclick="ajaxLoad('{{Request::url()}}?search='+this.value+'&sort={{$sort}}&field={{$sortField}} ')">
+                        <i class="icon search"></i>
+                    </button>
+                    <button class="ui icon button"
+                            onclick="ajaxLoad('{{Request::url()}}?search=')">
+                        <i class="icon trash"></i>
+                    </button>
+
+                </div>
+                <div class="results"></div>
+            </div>
+        </div>
+    </div>
+</div>
